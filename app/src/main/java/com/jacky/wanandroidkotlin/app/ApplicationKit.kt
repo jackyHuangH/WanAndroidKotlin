a978c17dd8a675067fff9ea78a41ba78fe1e018f
huangzijie <huangzijie@zenchn.com>
2019-06-20 17:41:52 +0800

    先熟悉MVVM架构和kotlin回顾再开写

---
+++ app/src/main/java/com/jacky/wanandroidkotlin/app/ApplicationKit.kt
@@ -0,0 +1,102 @@
+package com.jacky.wanandroidkotlin.app
+
+import android.app.Activity
+import android.app.NotificationManager
+import android.content.Intent
+import com.jacky.wanandroidkotlin.MainActivity
+import com.jacky.wanandroidkotlin.R
+import com.jacky.wanandroidkotlin.common.Constant
+import com.zenchn.apilib.base.ApiManager
+import com.zenchn.apilib.util.LoggerKit
+import com.zenchn.support.base.AbstractApplicationKit
+import com.zenchn.support.base.ActivityLifecycleCallback
+import com.zenchn.support.dafault.DefaultActivityLifecycle
+import com.zenchn.support.widget.tips.SuperToast
+
+
+/**
+ * @author:Hzj
+ * @date  :2018/10/30/030
+ * desc  ：
+ * record：
+ */
+class ApplicationKit private constructor() : AbstractApplicationKit(), ActivityLifecycleCallback {
+
+    private val mLazyActivityLifecycle: DefaultActivityLifecycle by lazy {
+        DefaultActivityLifecycle.getInstance()
+    }
+
+    fun exitApp() {
+        mLazyActivityLifecycle.exitApp()
+    }
+
+    fun navigateToLogin(grantRefuse: Boolean) {
+        if (grantRefuse) {
+            SuperToast.showDefaultMessage(
+                getApplication(),
+                getApplication().getString(R.string.login_error_grant_refused)
+            )
+        }
+        val topActivity = mLazyActivityLifecycle.topActivity
+        if (topActivity != null) {
+//            LoginActivity
+//                    .launch(topActivity)
+        } else {
+            val intent = Intent(application, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            application.startActivity(intent)
+        }
+    }
+
+    override fun initSetting() {
+        super.initSetting()
+        initApiManager()
+        LoggerKit.init(Constant.LOGGERKIT_TAG)
+        initActivityLifecycle()
+        clearNotify()
+    }
+
+    private fun initApiManager() {
+        ApiManager.init(Constant.BASE_REQUEST_HTTP_HOST, application)
+    }
+
+    private fun initActivityLifecycle() {
+        mLazyActivityLifecycle.addCallback(this)
+    }
+
+    /**
+     * 清理通知栏
+     */
+    private fun clearNotify() {
+        val nm = application.getSystemService(Activity.NOTIFICATION_SERVICE) as NotificationManager
+        nm.cancelAll()
+    }
+
+    override fun onCrash(thread: Thread?, ex: Throwable?) {
+        mLazyActivityLifecycle.exitApp()
+    }
+
+    override fun onBackground() {
+
+    }
+
+    override fun onForeground() {
+
+    }
+
+    override fun onDestroyedSelf() {
+
+    }
+
+
+    private object SingletonInstance {
+        internal val INSTANCE = ApplicationKit()
+    }
+
+    //伴生类，对象为静态单例
+    companion object {
+        val instance: ApplicationKit
+            get() = SingletonInstance.INSTANCE
+    }
+
+}
