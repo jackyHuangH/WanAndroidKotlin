c44e6fea16fafcc2278ef989cc7ed22177f7f539
huangzijie <huangzijie@zenchn.com>
2019-06-27 17:44:11 +0800

    协程高阶函数和上下文等学习

--- app/src/test/java/com/jacky/wanandroidkotlin/CoroutineTest.kt
+++ app/src/test/java/com/jacky/wanandroidkotlin/CoroutineTest.kt
@@ -1,10 +1,8 @@
 package com.jacky.wanandroidkotlin
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.junit.Test
+import kotlin.coroutines.CoroutineContext
 import kotlin.system.measureTimeMillis
 
 /**
@@ -15,11 +13,26 @@ import kotlin.system.measureTimeMillis
  * 在 Kotlin 中有一个约定：如果函数的最后一个参数接受函数，那么作为相应参数传入的 lambda 表达式可以放在圆括号之外(即在{}中)
  * 如果该 lambda 表达式是调用时唯一的参数，那么圆括号可以完全省略
  */
-class CoroutineTest {
+object CoroutineTest {
     @Test
     fun testMySuspendingFunction() = runBlocking<Unit> {
         // 这里我们可以通过任何我们喜欢的断言风格使用挂起函数
+    }
 
+    @Test
+    fun testChangeThread() {
+        //线程间切换,withContext函数切换协程的上下文，但依然是在相同的协程中执行
+        newSingleThreadContext("ctx1").use { ctx1 ->
+            newSingleThreadContext("ctx2").use { ctx2 ->
+                runBlocking(ctx1) {
+                    log("started in ctx1")
+                    withContext(ctx2) {
+                        log("work in ctx2")
+                    }
+                    log("back to ctx1")
+                }
+            }
+        }
     }
 
 }
@@ -40,19 +53,138 @@ fun calTwoAsync() = GlobalScope.async {
     calTwo()
 }
 
-fun main(args: Array<String>) = runBlocking<Unit> {
+//调试协程，打印协程名称
+fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
+
+//共享可变状态与并发
+suspend fun massiveRun(context: CoroutineContext, action: suspend () -> Unit) {
+    val n = 1000 // 启动协程的数量
+    val k = 1000 // 每个协程执行动作的次数
+    val time = measureTimeMillis {
+        val jobs = List(n) {
+            GlobalScope.launch(context) {
+                repeat(k) { action() }
+            }
+        }
+        jobs.forEach { it.join() }
+    }
+    println("Completed ${n * k} actions in $time ms")
+}
+
+val mtContext = newFixedThreadPoolContext(2, "mtPool") // 定义一个2线程的上下文
+
+fun main() = runBlocking<Unit> {
+    //共享可变状态与并发
+    var num = 0
+    massiveRun(mtContext) {
+        num++
+    }
+    println("num=$num")
+    //------------------------------------------
+
+    //通道Channel,延期的值提供了一种便捷的方法使单个值在多个协程之间进行相互传输。 通道提供了一种 *在流中传输值的方法*
+    //一个 Channel 是一个和 BlockingQueue 非常相似的概念。其中一个不同是它代替了阻塞的 put 操作并提供了挂起的 send，
+    // 还替代了阻塞的 take 操作并提供了挂起的 receive
+//    val channel = Channel<Int>()
+//    launch {
+//        // 这里可能是消耗大量 CPU 运算的异步逻辑，我们将仅仅做 5 次整数的平方并发送
+//        for (x in 1..5) {
+//            channel.send(x * x)
+//            if (x > 3) {
+//                //关闭通道，结束发送
+//                channel.close()
+//            }
+//        }
+//    }
+    // 这里我们打印了 5 次被接收的整数：
+//    repeat(5) {
+//        println(channel.receive())
+//    }
+    // 这里我们使用 `for` 循环来打印所有被接收到的元素（直到通道被关闭）
+//    for (y in channel) println(y)
+//    println("done!")
+
+    //-------------------------------------------------------
+
+    //子协程当一个协程被其它协程在 CoroutineScope 中启动的时候， 它将通过 CoroutineScope.coroutineContext 来承袭上下文，
+    // 并且这个新协程的 Job 将会成为父协程作业的 子 作业。当一个父协程被取消的时候，所有它的子协程也会被递归的取消。
+//    val fatherRequest = launch {
+//        // 孵化了两个子作业, 其中一个通过 GlobalScope 启动
+//        GlobalScope.launch {
+//            println("job1: I run in GlobalScope and execute independently!")
+//            delay(1000)
+//            println("job1: I am not affected by cancellation of the request")
+//        }
+//        //另一个则承袭了父协程的上下文
+//        launch {
+//            delay(100)
+//            println("job2: I am a child of the request coroutine")
+//            delay(1000)
+//            println("job2: I will not execute this line if my parent request is cancelled")
+//        }
+//        coroutineScope {  }
+//        MainScope()
+//    }
+//    delay(500)
+//    fatherRequest.cancel()//取消request请求
+//    delay(1000)
+//    println("main: Who has survived request cancellation?")
+    //-------------------------------------
+//    CoroutineTest.testChangeThread()
+
+    //------------------------------------------------
+    //调试协程和线程
+//    val a = async(coroutineContext) {
+//        log("I am doing something")
+//        6
+//    }
+//    val b = async(coroutineContext) {
+//        log("I am doing some other thing")
+//        9
+//    }
+//    println("My job is ${coroutineContext[Job]}")
+//    log("the answer is ${a.await()+b.await()}")
+    //-----------------------------------------------------
+
+    //-调度器
+//    val job1 = GlobalScope.launch {
+//        // 运行在父协程的上下文中，即 runBlocking 主协程
+//        println("main runBlocking:current thread is ${Thread.currentThread().name}")
+//        delay(1000L)
+//        println("main runBlocking:after delay current thread is ${Thread.currentThread().name}")
+//    }
+//
+//    val job2 = GlobalScope.launch(Dispatchers.Unconfined) {
+//        // 不受限的,将工作在主线程中
+//        println("Unconfined:current thread is ${Thread.currentThread().name}")
+//        delay(500L)
+//        println("Unconfined:after delay current thread is ${Thread.currentThread().name}")
+//    }
+//    job1.join()
+//    job2.join()
+
+    /* GlobalScope.launch(Dispatchers.Default) {
+         // 将会获取默认调度器
+         println("Default:current thread is ${Thread.currentThread().name}")
+     }
+     GlobalScope.launch(newSingleThreadContext("newThread")) {
+         //获得一个新的线程
+         println("newSingleThreadContext:current thread is ${Thread.currentThread().name}")
+     }*/
+
+    //--------------------------------------------------------
 
     //函数类型实例化
-    val stringPlus: (String, String) -> String = String::plus
-    val intPlus: Int.(Int) -> Int = Int::plus
-
-    //函数类型的值可以通过其 invoke(……) 操作符调用：f.invoke(x) 或者直接 f(x)
-    println(stringPlus("--", "22"))
-    println(stringPlus.invoke("nihao", "背景"))
-    println(intPlus(3, 9))
-    println(intPlus.invoke(4, 9))
-    //// 类扩展调用
-    println(8.intPlus(9))
+//    val stringPlus: (String, String) -> String = String::plus
+//    val intPlus: Int.(Int) -> Int = Int::plus
+//
+//    //函数类型的值可以通过其 invoke(……) 操作符调用：f.invoke(x) 或者直接 f(x)
+//    println(stringPlus("--", "22"))
+//    println(stringPlus.invoke("nihao", "背景"))
+//    println(intPlus(3, 9))
+//    println(intPlus.invoke(4, 9))
+//    //// 类扩展调用
+//    println(8.intPlus(9))
 
     //--------------------------------------------------------------------------
 
@@ -71,18 +203,18 @@ fun main(args: Array<String>) = runBlocking<Unit> {
     // 表示一会就会返回结果的承诺。你可以在一个延期的值（deferred value）使用.await()来获取最终的结果，
     // 但Deferred也是个Job， 因此，需要的话，你也可以取消掉。
     //
-    val time = measureTimeMillis {
-        //协程并发执行
-//        val calOne = async(start = CoroutineStart.LAZY) { calOne() }
-//        val calTwo = async(start = CoroutineStart.LAZY) { calTwo() }
-
-        val calOneAsync = calOneAsync()
-        val calTwoAsync = calTwoAsync()
-        //执行异步方法时，要在runBlock{}阻塞主线程来获取结果
-
-        println("cal total:${calOneAsync.await() + calTwoAsync.await()}")
-    }
-    println("time:$time")
+//    val time = measureTimeMillis {
+//        //协程并发执行
+////        val calOne = async(start = CoroutineStart.LAZY) { calOne() }
+////        val calTwo = async(start = CoroutineStart.LAZY) { calTwo() }
+//
+//        val calOneAsync = calOneAsync()
+//        val calTwoAsync = calTwoAsync()
+//        //执行异步方法时，要在runBlock{}阻塞主线程来获取结果
+//
+//        println("cal total:${calOneAsync.await() + calTwoAsync.await()}")
+//    }
+//    println("time:$time")
 
 
     // 在后台启动一个新的协程，然后继续执行
